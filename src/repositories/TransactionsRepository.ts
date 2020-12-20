4e09ba0271fae987a9ba741626cfe1e2f1337800
lstaborda <leticia.s.taborda@gmail.com>
2020-12-20 20:52:00 -0300

    Add POST, GET and validation functions to transactions API

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,28 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((prev, current) => {
+      return current.type === 'income' ? prev + current.value : prev;
+    }, 0);
+
+    const outcome = this.transactions.reduce((prev, current) => {
+      return current.type === 'outcome' ? prev + current.value : prev;
+    }, 0);
+
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
